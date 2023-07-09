abc72cc32b90c85b743855c3083acb95e875dcc5
TaiChi31 <tung3108vn@gmail.com>
2023-07-10 03:48:23 +0700

    update

--- main.py
+++ main.py
@@ -1,31 +1,71 @@
+
+import time
 import cv2
-from tkinter import *
-
-# Khởi tạo đối tượng VideoCapture để truy cập webcam
-cap = cv2.VideoCapture("rtsp://192.168.31.86:8554/mystream")  # Số 0 đại diện cho webcam mặc định
-
-# Kiểm tra xem webcam đã sẵn sàng hay chưa
-if not cap.isOpened():
-    print("Không thể mở webcam")
-    exit()
-
-# Vòng lặp để đọc và hiển thị video từ webcam
-while True:
-    # Đọc frame từ webcam
-    ret, frame = cap.read()
-
-    # Kiểm tra xem việc đọc frame có thành công hay không
-    if not ret:
-        print("Không thể đọc frame từ webcam")
-        break
-
-    # Hiển thị frame
-    cv2.imshow('Webcam', frame)
-
-    # Thoát vòng lặp khi nhấn phím 'q'
-    if cv2.waitKey(1) == ord('q'):
-        break
-
-# Giải phóng webcam và đóng cửa sổ hiển thị
-cap.release()
-cv2.destroyAllWindows()
+import threading
+import datetime
+import time
+import tkinter
+import PIL
+import PIL.Image, PIL.ImageTk
+from screeninfo import get_monitors
+class status_webcam():
+    def __init__(self,adress) -> None:
+        self.adress = adress
+        self.status_camera()
+    def process_video(self,camera) -> tuple[bool,any]:
+        (self.grabbed, self.frame) = camera.read()
+        if not self.grabbed:
+            print("disconnected!")
+            return[self.grabbed,self.frame]
+        else:
+            #print("get frame")
+            return [self.grabbed,self.frame]
+    def status_camera(self):
+        a = gui()
+        while 1:
+            camera = cv2.VideoCapture(self.adress)
+            if camera.isOpened():
+                print("[INFO] Camera connected at " +
+                    datetime.datetime.now().strftime("%m-%d-%Y %I:%M:%S%p"))
+                while 1:
+                    x,frame = self.process_video(camera)
+                    a.frame = frame
+                    a.x = x
+                    if x == False:
+                        break
+            else:
+                print("Camera not opened " +
+                    datetime.datetime.now().strftime("%m-%d-%Y %I:%M:%S%p"))
+                a.x = False
+                a.frame = None
+                time.sleep(5)
+                camera.release()
+class gui():
+    def __init__(self,frame=None,x=False) -> None:
+        self.x = x
+        self.frame = frame
+        t_show = threading.Thread(target=self.show)
+        t_show.start()
+    def show(self):
+        self.root = tkinter.Tk()
+        self.root.attributes("-fullscreen", True) 
+        self.canvas = tkinter.Canvas(self.root)
+        self.canvas.pack(expand=True, fill=tkinter.BOTH)
+        self.update()
+        self.root.mainloop()
+    def update(self):
+        if self.x:
+            self.photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(self.frame))
+            # self.canvas.create_image(0, 0, image = self.photo, anchor = tkinter.NW)
+        else:
+            self.photo = self.fill_image()
+        self.canvas.create_image(0, 0, image = self.photo, anchor = tkinter.NW)
+        self.root.after(1, self.update)
+    def fill_image(self):
+        a = get_monitors()
+        scr_img = PIL.Image.open('colorbar.png')
+        return PIL.ImageTk.PhotoImage(scr_img.resize((a[0].width,a[0].height)))  
+def status_():
+    status_webcam("rtsp://192.168.31.86:8554/mystream")
+t_status = threading.Thread(name="status_camera",target=status_)
+t_status.start()
