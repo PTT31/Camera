0e5d3abbe25146890e3a6002630ca2a99fcc1a8a
TaiChi31 <tung3108vn@gmail.com>
2023-07-10 06:51:10 +0700

    update

--- main.py
+++ main.py
@@ -28,7 +28,10 @@ class status_webcam():
                     datetime.datetime.now().strftime("%m-%d-%Y %I:%M:%S%p"))
                 while 1:
                     x,frame = self.process_video(camera)
-                    a.frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+                    try:
+                        a.frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+                    except:
+                        break
                     a.x = x
                     if x == False:
                         break
@@ -40,32 +43,43 @@ class status_webcam():
                 a.frame = None
                 time.sleep(5)
                 camera.release()
+            continue
 class gui():
     def __init__(self,frame=None,x=False) -> None:
         self.x = x
         self.frame = frame
         self.fps = 30
+        a = get_monitors()
+        self.rsize = (a[0].width,a[0].height)
+        
         t_show = threading.Thread(target=self.show)
         t_show.start()
+
     def show(self):
         self.root = tkinter.Tk()
         self.root.attributes("-fullscreen", True) 
         self.canvas = tkinter.Canvas(self.root)
         self.canvas.pack(expand=True, fill=tkinter.BOTH)
+        self.bg = self.canvas.create_image(0, 0, anchor = tkinter.NW,)
+        self.image_erorr = self.fill_image()
         self.update()
         self.root.mainloop()
     def update(self):
         if self.x:
             self.photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(self.frame))
+            self.canvas.itemconfig(self.bg,image = self.photo)
+            self.canvas.pack(fill="both",expand=True)
+            time.sleep(1/self.fps)
+            self.root.after(1, self.update)
         else:
-            self.photo = self.fill_image()
-        self.canvas.create_image(0, 0, image = self.photo, anchor = tkinter.NW)
-        time.sleep(1/self.fps)
-        self.root.after(1, self.update)
+            
+            self.canvas.itemconfig(self.bg,image = self.image_erorr)
+            self.canvas.pack(fill="both",expand=True)
+            time.sleep(1)      
+            self.root.after(5, self.update)
     def fill_image(self):
-        a = get_monitors()
         scr_img = PIL.Image.open('colorbar.png')
-        return PIL.ImageTk.PhotoImage(scr_img.resize((a[0].width,a[0].height)))  
+        return PIL.ImageTk.PhotoImage(scr_img.resize(self.rsize))  
 def status_():
     status_webcam("rtsp://192.168.31.86:8554/mystream")
 t_status = threading.Thread(name="status_camera",target=status_)
